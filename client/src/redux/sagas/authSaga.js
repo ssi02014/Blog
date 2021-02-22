37a0b1a5dec4d483f907cb3a85b163cdf759bee4
JeonMinJae <ssi02014@naver.com>
2021-02-23 00:41:34 +0900

    Add(minjae/client): Auth Redux Saga 작성

---
+++ client/src/redux/sagas/authSaga.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { all, fork, put, takeEvery, call } from 'redux-saga/effects';
+import { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS } from '../types';
+
+//Login
+function loginUserAPI(loginData) {
+    console.log(loginData, "loginData");
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+    return axios.post('/api/auth', loginData, config);
+}
+
+function* loginUser(action) {
+    try {
+        const result = yield call(loginUserAPI, action.payload);
+
+        console.log(result);
+
+        yield put({
+            type: LOGIN_SUCCESS,
+            payload: result.data
+        });
+    } catch (e) {
+        yield put({
+            type: LOGIN_FAILURE,
+            payload: e.response,
+        });
+    }
+}
+
+function* watchLoginUser() {
+    yield takeEvery(LOGIN_REQUEST, loginUser);
+}
+
+export default function* authSaga() {
+    yield all([
+        fork(watchLoginUser)
+    ]);
+}
