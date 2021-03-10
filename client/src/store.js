f19c78852b217cff97e4782768d3f8cb63dd724d
JeonMinJae <ssi02014@naver.com>
2021-03-11 00:54:54 +0900

    Add(minjae/server): Post Delete API 구현

--- client/src/store.js
+++ client/src/store.js
@@ -5,11 +5,12 @@ import { routerMiddleware } from 'connected-react-router';
 import createRootReducer from './redux/reducers/index';
 import rootSaga from './redux/sagas';
 
+export const history = createBrowserHistory();
+
 const sagaMiddleware = createSagaMiddleware();
+
 const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 const composeEnhancer = process.env.NODE_ENV === "production" ? compose : devtools || compose;
-
-export const history = createBrowserHistory();
 
 const initialState = {};
 
--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -182,4 +182,27 @@ router.post("/:id/comments", async (req, res) => {
         next(e);
     }
 });
+
+//api/post/:id
+router.delete("/:id", auth, async(req, res) => {
+    await Post.deleteMany({_id: req.params.id});
+    await Comment.deleteMany({post: req.params.id});
+    await User.findByIdAndUpdate(req.user.id, {
+        $pull: {
+            posts: req.params.id,
+            comments: { post_id: req.params.id },
+        },
+    });
+
+    const CategoryUpdateResult = await Category.findByIdAndUpdate(
+        {posts: req.params.id },
+        {$pull: { posts: req.params.id }},
+        {new: true },
+    );
+
+    if(CategoryUpdateResult.posts.length === 0) {
+        await Category.deleteMany({ _id: CategoryUpdateResult });
+    }
+    return res.json({success: true});
+});
 export default router;
