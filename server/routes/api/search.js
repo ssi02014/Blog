2764512b42701e97d930dfb2cb06665f05cbfa8d
JeonMinJae <ssi02014@naver.com>
2021-06-08 01:37:19 +0900

    Add(minjae/search): Search Sever 작업 #15

---
+++ server/routes/api/search.js
@@ -0,0 +1,25 @@
+import express from "express";
+import Post from "../../models/post";
+
+const router = express.Router();
+
+router.get("/:searchTerm", async (req, res, next) => {
+  try {
+    const result = await Post.find(
+      {
+        title: {
+          $regex: req.params.categoryName,
+          $options: "i",
+        },
+      },
+      "posts"
+    );
+    console.log(result, "Search result");
+    res.send(result);
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
+
+export default router;
