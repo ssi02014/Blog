72d8e27d745036d89b6f128cd20da98c3a1bc984
JeonMinJae <ssi02014@naver.com>
2021-06-11 01:23:14 +0900

    Add(minjae/profile): profile Server #14

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -1,70 +1,106 @@
-import express from 'express';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import config from '../../config/index';
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import config from "../../config/index";
 
-import User from '../../models/user'; //Model
+import User from "../../models/user"; //Model
 
 const { JWT_SECRET } = config;
 const router = express.Router();
 
-router.get('/', async(req, res) => {
-    try {
-        const users = await User.find()
-        if (!users) throw Error("No users");
-        res.status(200).json(users);
-    } catch(e) {
-        console.log(e);
-        res.status(400).json({ msg: e.message })
-    }
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    if (!users) throw Error("No users");
+    res.status(200).json(users);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ msg: e.message });
+  }
 });
 
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const { name, email, password } = req.body;
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const { name, email, password } = req.body;
+  //Simple Validation
+  if (!name || !email || !password) {
+    return res.status(400).json({ msg: "모든 필드를 채워주세요." });
+  }
 
-    //Simple Validation
-    if (!name || !email || !password) {
-        return res.status(400).json({ msg: "모든 필드를 채워주세요." });
-    }
+  User.findOne({ email }).then((user) => {
+    if (user)
+      return res.status(400).json({ msg: "이미 가입된 유저가 존재합니다." });
 
-    User.findOne({ email })
-        .then(user => {
-            if (user) return res.status(400).json({ msg: "이미 가입된 유저가 존재합니다." });
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
 
-            const newUser = new User({
-                name, email, password
-            });
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
 
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    if (err) throw err;
+        newUser.password = hash;
 
-                    newUser.password = hash;
+        newUser.save().then((user) => {
+          jwt.sign(
+            { id: user.id },
+            JWT_SECRET,
+            { expiresIn: 3600 },
+            (err, token) => {
+              if (err) throw err;
 
-                    newUser.save().then(user => {
-                        jwt.sign(
-                            { id: user.id },
-                            JWT_SECRET, 
-                            { expiresIn: 3600 },
-                            (err, token) => {
-                                if (err) throw err;
-
-                                res.json({
-                                    token,
-                                    user: {
-                                        id: user.id,
-                                        name: user.name,
-                                        email: user.email,
-                                    }
-                                });
-                            }
-                        );
-                    });
-                });
-            });
+              res.json({
+                token,
+                user: {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email,
+                },
+              });
+            }
+          );
         });
+      });
+    });
+  });
 });
 
-export default router
+router.post("/:userName/profile", async (req, res) => {
+  try {
+    const { previousPassword, password, rePassword, userId } = req.body;
+    console.log(req.body, "userName Profile");
+    const result = await User.findById(userId, "password");
+
+    bcrypt.compare(previousPassword, result.password).then((isMatch) => {
+      if (!isMatch) {
+        return res.status(400).json({
+          match_msg: "기존 비밀번호와 일치하지 않습니다.",
+        });
+      } else {
+        if (password === rePassword) {
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, (err, hash) => {
+              if (err) throw err;
+              result.password = hash;
+              result.save();
+            });
+          });
+          res
+            .status(200)
+            .json({ success_msg: "비밀번호 업데이트에 성공했습니다." });
+        } else {
+          res
+            .status(400)
+            .json({ fail_msg: "새로운 비밀번호가 일치하지 않습니다." });
+        }
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+export default router;
